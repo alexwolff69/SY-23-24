78b082836e1285e009bc5d518f83576b46d513c7
alex wolff <abwol538@learner.dvusd.org>
2024-03-21 13:53:55 -0700

    hope this works

--- project with daniel/project with daniel/Form1.vb
+++ project with daniel/project with daniel/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Hide()
+        Dim f2 As New form2
+        f2.showdialog
+        Label2.text = f2.tierscount.value
+        Me.Show()
+    End Sub
 End Class
